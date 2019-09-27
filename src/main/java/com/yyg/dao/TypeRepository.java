11f0d4b417bcd9dafe56029fb2c45ad387b45e46
yyuguang <790335255@qq.com>
2019-09-27 18:13:04 +0800

    test

--- src/main/java/com/yyg/dao/TypeRepository.java
+++ src/main/java/com/yyg/dao/TypeRepository.java
@@ -12,5 +12,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface TypeRepository extends JpaRepository<Type,Long> {
 
+    /**
+     * 查询
+     * @param name
+     * @return
+     */
     Type findByName(String name);
 }
